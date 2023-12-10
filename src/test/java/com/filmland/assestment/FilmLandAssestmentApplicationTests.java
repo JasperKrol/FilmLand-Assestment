ec2df46e89994b42f0b1d2d2e259738eae5cc233
Jasper Krol <jasper.krol@incentro.com>
2023-12-10 21:56:28 +0100

    removed

---
+++ src/test/java/com/filmland/assestment/FilmLandAssestmentApplicationTests.java
@@ -0,0 +1,12 @@
+package com.filmland.assestment;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmLandAssestmentApplicationTests {
+
+  
+
+}
